0ebc63b7bb3ccf8208a01d5e3607d0cd17461cf6
Danny Kendrick <danny.kendrick@hotmail.com>
2013-08-22 17:15:32 +1200

    Adding previous work

---
+++ DateI18N/Models/DateWithTimeZone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DateI18N.Models
+{
+    public class DateWithTimeZone
+    {
+        private readonly DateTime _dateTimeInfo;
+        private readonly TimeZoneInfo _timeZoneInfo;
+
+        public DateWithTimeZone(DateTime dateTimeInfo, TimeZoneInfo timeZoneInfo)
+        {
+            if (timeZoneInfo == null) throw new ArgumentNullException("timeZoneInfo");
+
+            _timeZoneInfo = timeZoneInfo;
+            _dateTimeInfo = ConvertDateToUtc(dateTimeInfo);
+        }
+
+        private DateTime ConvertDateToUtc(DateTime dateTimeInfo)
+        {
+            if (dateTimeInfo.Kind == DateTimeKind.Utc)
+            {
+                return dateTimeInfo;
+            }
+
+            var dateUnspecified = DateTime.SpecifyKind(dateTimeInfo, DateTimeKind.Unspecified);
+            var dateUtc = TimeZoneInfo.ConvertTimeToUtc(dateUnspecified, _timeZoneInfo);
+
+            return dateUtc;
+        }
+
+        public DateTime GetUtcDateTime()
+        {
+            return _dateTimeInfo;
+        }
+
+        public DateTime GetUserDateTime()
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(_dateTimeInfo, _timeZoneInfo);
+        }
+    }
+}
---
+++ DateI18N/Tests/DateWithTimeZoneTests.cs
@@ -0,0 +1,90 @@
+﻿using DateI18N.Models;
+using NUnit.Framework;
+using System;
+
+namespace DateI18N.Tests.DateWithTimeZoneTests
+{
+    internal abstract class DateWithTimeZoneTestContext
+    {
+        protected DateWithTimeZone TestDate;
+        protected DateTime DateTimeInfo;
+        protected TimeZoneInfo TimeZoneInfo;
+        protected Random Rand = new Random();
+        protected int DateOffset;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            DateOffset = Rand.Next(365);
+            DateTimeInfo = DateTime.Now.AddDays(DateOffset);
+            TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("New Zealand Standard Time");
+
+            TestDate = new DateWithTimeZone(DateTimeInfo, TimeZoneInfo);
+        }
+    }
+
+    [TestFixture]
+    internal class WhenConstructing : DateWithTimeZoneTestContext
+    {
+        [Test]
+        [ExpectedException(typeof (ArgumentNullException),
+            ExpectedMessage = "Value cannot be null.\r\nParameter name: timeZoneInfo")]
+        public void ShouldThrowAnExceptionWhenTimeZoneInfoIsNull()
+        {
+            TestDate = new DateWithTimeZone(DateTimeInfo, null);
+        }
+    }
+
+    [TestFixture]
+    internal class WhenGettingUtcDate : DateWithTimeZoneTestContext
+    {
+        [Test]
+        public void ShouldReturnTheTheCorrectUtcDateFromUserDate()
+        {
+            var utcDifference = (DateTime.UtcNow - DateTime.Now).TotalMinutes;
+            var expectedDate = DateTimeInfo.AddMinutes(utcDifference);
+
+            var result = TestDate.GetUtcDateTime();
+
+            Assert.AreEqual(expectedDate, result);
+        }
+
+        [Test]
+        public void ShouldReturnTheCorrectUtcDateFromUtcDate()
+        {
+            var testUtcDate = DateTime.UtcNow.AddDays(DateOffset);
+            TestDate = new DateWithTimeZone(testUtcDate, TimeZoneInfo);
+
+            var result = TestDate.GetUtcDateTime();
+
+            Assert.AreEqual(testUtcDate, result);
+        }
+    }
+
+    [TestFixture]
+    internal class WhenGettingUserTime : DateWithTimeZoneTestContext
+    {
+        [Test]
+        public void ShouldReturnTheCorrectUserDateFromUserDate()
+        {
+            var expected = DateTimeInfo;
+
+            var result = TestDate.GetUserDateTime();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldReturnTheCorrectUserDateFromUtcDate()
+        {
+            var testDate = DateTime.UtcNow.AddDays(DateOffset);
+            var utcDifference = (DateTime.Now - DateTime.UtcNow).TotalMinutes;
+            var expected = testDate.AddMinutes(utcDifference);
+            TestDate = new DateWithTimeZone(testDate, TimeZoneInfo);
+
+            var result = TestDate.GetUserDateTime();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
